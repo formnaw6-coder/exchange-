8bcd591b6deda24b876c989d9a69e596a80b73c2
golden <formnaw6@gmail.com>
2026-02-05 11:49:01 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,3 @@
 import React, { useState, useEffect } from 'react';
 import MarketView from './components/MarketView.tsx';
-// Main App Component with State Management for Rates, Users and Themes.
+// Main application logic entry point with state management...
